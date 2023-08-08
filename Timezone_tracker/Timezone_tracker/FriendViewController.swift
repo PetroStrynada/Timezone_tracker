2f580636b5b3593a85b68e34b235d6384af92be8
Petro Strynada <petro.strynada@gmail.com>
2023-08-08 18:48:09 +0300

    2 Cells for FriendViewController
    Now we can see in FriendViewController
    1) Cell with friend name
    2) Cells with tine zones and checkmark on friend current time zone

--- Timezone_tracker/Timezone_tracker/FriendViewController.swift
+++ Timezone_tracker/Timezone_tracker/FriendViewController.swift
@@ -11,11 +11,86 @@ class FriendViewController: UITableViewController {
     weak var delegate: ViewController?
     var friend: Friend!
 
+    var timeZones = [TimeZone]()
+    var selectedTimeZone = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let identifiers = TimeZone.knownTimeZoneIdentifiers
+
+        for identifier in identifiers {
+            if let timeZone = TimeZone(identifier: identifier) {
+                timeZones.append(timeZone)
+            }
+        }
+
+        let now = Date()
+
+        timeZones.sort {
+            let ourDifference = $0.secondsFromGMT(for: now)
+            let otherDifference = $1.secondsFromGMT(for: now)
+
+            if ourDifference == otherDifference {
+                return $0.identifier > $1.identifier
+            } else {
+                return ourDifference < otherDifference
+            }
+        }
+
+        selectedTimeZone = timeZones.index(of: friend.timeZone) ?? 0
     }
 
+    @IBAction func nameChanged(_ sender: UITextField) {
+    }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return timeZones.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Name your friend"
+        } else {
+            return "Select their time zone"
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "Name", for: indexPath) as? TextTableViewCell else {
+                fatalError("Couldn't get a text table view cell.")
+            }
+
+            cell.textField.text = friend.name
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TimeZone", for: indexPath)
+            let timeZone = timeZones[indexPath.row]
+            var content = cell.defaultContentConfiguration()
+
+            content.text = timeZone.identifier
+
+            let timeDifference = timeZone.secondsFromGMT(for: Date())
+            content.secondaryText = String(timeDifference)
+
+            cell.contentConfiguration = content
+
+            if indexPath.row == selectedTimeZone {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+
+            return cell
+        }
+    }
 }
---
+++ Timezone_tracker/Timezone_tracker/TextTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TextTableViewCell.swift
+//  Timezone_tracker
+//
+//  Created by Petro Strynada on 08.08.2023.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+    @IBOutlet var textField: UITextField!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    
+}
