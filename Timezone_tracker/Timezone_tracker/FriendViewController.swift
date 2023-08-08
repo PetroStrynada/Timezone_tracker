4d49b4a1055ed9a894669f348ecc10e6b7802950
Petro Strynada <petro.strynada@gmail.com>
2023-08-08 16:59:41 +0300

    Screen for friend configuration
    Now, when tapping + button or cell with friend
    vc push for friend configuration screen
    
    + FriendViewController
    + func configure
    + didSelectRowAt

---
+++ Timezone_tracker/Timezone_tracker/FriendViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FriendViewController.swift
+//  Timezone_tracker
+//
+//  Created by Petro Strynada on 08.08.2023.
+//
+
+import UIKit
+
+class FriendViewController: UITableViewController {
+    weak var delegate: ViewController?
+    var friend: Friend!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
