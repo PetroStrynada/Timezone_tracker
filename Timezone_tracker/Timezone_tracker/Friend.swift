139795cca902e7d5ee20772fd10c0f970088481e
Petro Strynada <petro.strynada@gmail.com>
2023-08-08 16:23:37 +0300

    Cells with friends
    With each new friend at the top will appear a new row with the friend's
    name left and time zone wright
    + TableViewController
    + struct Friend
    + title "Friend zone"
    + right bar button for adding new friends
    + save friends with JSONEncoder and load with JSONDecoder

---
+++ Timezone_tracker/Timezone_tracker/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  Timezone_tracker
+//
+//  Created by Petro Strynada on 08.08.2023.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    var name = "New friend"
+    var timeZone = TimeZone.current
+}
