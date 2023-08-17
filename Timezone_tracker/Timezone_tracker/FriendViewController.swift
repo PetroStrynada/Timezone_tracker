a95c5bf01bc27bcd78d409c9be7f868a5e05092a
Petro Strynada <petro.strynada@gmail.com>
2023-08-17 18:10:03 +0300

    BUGFIX friend update
    Now viewWillDisappear in TimeZoneViewController and FriendViewController
    Save and update timeZone correctly
    
    The selection of the time zone was finally transferred to
    TimeZoneViewController

--- Timezone_tracker/Timezone_tracker/FriendViewController.swift
+++ Timezone_tracker/Timezone_tracker/FriendViewController.swift
@@ -48,7 +48,140 @@ class FriendViewController: UITableViewController, Storyboarded, CoordinatedFrie
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        coordinator?.update(friend: friend)
+        coordinator?.updateViewController(friend: friend)
+    }
+
+    @IBAction func nameChanged(_ sender: UITextField) {
+        friend.name = sender.text ?? ""
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Name"
+        } else {
+            return "Current time zone"
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return 1
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "Name", for: indexPath) as? TextTableViewCell else {
+                fatalError("Couldn't get a text table view cell.")
+            }
+
+            cell.textField.text = friend.name
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentTimeZone", for: indexPath)
+            let currentTimeZone = friend.timeZone
+            var content = cell.defaultContentConfiguration()
+
+            content.text = currentTimeZone.identifier.replacingOccurrences(of: "_", with: " ")
+
+            let timeDifference = currentTimeZone.secondsFromGMT(for: Date())
+            content.secondaryText = timeDifference.timeString()
+
+            cell.contentConfiguration = content
+
+            return cell
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        if indexPath.section == 0 {
+            startEditingName()
+        } else {
+            coordinator?.showViewController(TimeZoneViewController.self, for: friend)
+        }
+    }
+
+    func startEditingName() {
+        nameEditionCell?.textField.becomeFirstResponder()
+    }
+
+    func upDateFriendViewController(updateFriend: Friend) {
+
+        friend.timeZone = updateFriend.timeZone
+        tableView.reloadData()
+        //coordinator?.saveData(friends, forKey: "Friends", errorMessage: "Unable to encode friends data.")
+    }
+
+    func selectRow(at indexPath: IndexPath) {
+
+        nameEditionCell?.textField.resignFirstResponder()
+        tableView.reloadData()
+    }
+}
+
+
+
+
+
+/*
+//
+//  FriendViewController.swift
+//  Timezone_tracker
+//
+//  Created by Petro Strynada on 08.08.2023.
+//
+
+import UIKit
+
+class FriendViewController: UITableViewController, Storyboarded, CoordinatedFriend {
+    weak var coordinator: MainCoordinator?
+    var friend: Friend!
+
+    var timeZones = [TimeZone]()
+    var selectedTimeZone = 0
+
+    var nameEditionCell: TextTableViewCell? {
+        let indexPath = IndexPath(row: 0, section: 0)
+        return tableView.cellForRow(at: indexPath) as? TextTableViewCell
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let identifiers = TimeZone.knownTimeZoneIdentifiers
+
+        for identifier in identifiers {
+            if let timeZone = TimeZone(identifier: identifier) {
+                timeZones.append(timeZone)
+            }
+        }
+
+        let now = Date()
+
+        timeZones.sort {
+            let ourDifference = $0.secondsFromGMT(for: now)
+            let otherDifference = $1.secondsFromGMT(for: now)
+
+            if ourDifference == otherDifference {
+                return $0.identifier > $1.identifier
+            } else {
+                return ourDifference < otherDifference
+            }
+        }
+
+        selectedTimeZone = timeZones.firstIndex(of: friend.timeZone) ?? 0
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        coordinator?.updateViewController(friend: friend)
     }
 
     @IBAction func nameChanged(_ sender: UITextField) {
@@ -88,18 +221,18 @@ class FriendViewController: UITableViewController, Storyboarded, CoordinatedFrie
             cell.textField.text = friend.name
             return cell
         } else if indexPath.section == 1 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentTimeZone", for: indexPath)
-            let currentTimeZone = friend.timeZone
-            var content = cell.defaultContentConfiguration()
+                let cell = tableView.dequeueReusableCell(withIdentifier: "CurrentTimeZone", for: indexPath)
+                let currentTimeZone = friend.timeZone
+                var content = cell.defaultContentConfiguration()
 
-            content.text = currentTimeZone.identifier.replacingOccurrences(of: "_", with: " ")
+                content.text = currentTimeZone.identifier.replacingOccurrences(of: "_", with: " ")
 
-            let timeDifference = currentTimeZone.secondsFromGMT(for: Date())
-            content.secondaryText = timeDifference.timeString()
+                let timeDifference = currentTimeZone.secondsFromGMT(for: Date())
+                content.secondaryText = timeDifference.timeString()
 
-            cell.contentConfiguration = content
+                cell.contentConfiguration = content
 
-            return cell
+                return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "TimeZone", for: indexPath)
             let timeZone = timeZones[indexPath.row]
@@ -139,6 +272,14 @@ class FriendViewController: UITableViewController, Storyboarded, CoordinatedFrie
         nameEditionCell?.textField.becomeFirstResponder()
     }
 
+    func upDateFriendViewController(updateFriend: Friend) {
+
+        friend.timeZone = updateFriend.timeZone
+        tableView.reloadData()
+        //coordinator?.saveData(friends, forKey: "Friends", errorMessage: "Unable to encode friends data.")
+    }
+
+    //TODO: Delete
     func changeTimeZoneAlert(at indexPath: IndexPath) {
         let timeZone = timeZones[indexPath.row]
         let zoneRegion = timeZone.identifier.replacingOccurrences(of: "_", with: " ")
@@ -156,8 +297,9 @@ class FriendViewController: UITableViewController, Storyboarded, CoordinatedFrie
     func selectRow(at indexPath: IndexPath) {
         nameEditionCell?.textField.resignFirstResponder()
 
+        //TODO: Delete
+        //unchecking cells
         for cell in tableView.visibleCells {
-            //unchecking cells
             cell.accessoryType = .none
         }
 
@@ -170,3 +312,4 @@ class FriendViewController: UITableViewController, Storyboarded, CoordinatedFrie
         tableView.reloadData()
     }
 }
+*/
