5d8f50b2a93c98bbbd3e69fd6a209a7880de1226
Petro Strynada <petro.strynada@gmail.com>
2023-08-20 15:56:31 +0300

    Divided storyboard
    Divided "Main" storyboard on one storyboard for each VC

--- Timezone_tracker/Timezone_tracker/Screens/FriendZone/FriendZoneViewController.swift
+++ Timezone_tracker/Timezone_tracker/Screens/FriendZone/FriendZoneViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController, Storyboarded {
+class FriendZoneViewController: UITableViewController, Storyboarded {
     weak var coordinator: MainCoordinator?
     var friends = [Friend]()
     var selectedFriend: Int? = nil
@@ -15,16 +15,24 @@ class ViewController: UITableViewController, Storyboarded {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Register the cell class or nib here
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        setupNavigationBar()
+        loadFriends()
+        setupTableView()
+    }
 
+    private func setupNavigationBar() {
+        title = "Friend Zone"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
+    }
+
+    private func loadFriends() {
         if let savedFriends: [Friend] = coordinator?.loadData(forKey: "Friends") {
             friends = savedFriends
         }
+    }
 
-        title = "Friend Zone"
-
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
+    private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "FriendZone")
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,30 +40,21 @@ class ViewController: UITableViewController, Storyboarded {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var cell = tableView.dequeueReusableCell(withIdentifier: "FriendZone", for: indexPath)
         let friend = friends[indexPath.row]
 
-        // Configure the cell style to "Right Detail"
-        cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
-
-        // Chevron at right side
+        cell = UITableViewCell(style: .value1, reuseIdentifier: "FriendZone")
         cell.accessoryType = .disclosureIndicator
-
-        // Step 1: Create a custom content configuration
         var content = cell.defaultContentConfiguration()
 
-        // Step 2.1: Set the primary text in the content configuration
         content.text = friend.name
 
         let dateFormatter = DateFormatter()
         dateFormatter.timeZone = friend.timeZone
         dateFormatter.timeStyle = .short
 
-        // Step 2.2: Set the secondary text in the content configuration
-        //content.secondaryText = friend.timeZone.identifier
         content.secondaryText = dateFormatter.string(from: Date())
 
-        // Step 3: Apply the custom configuration to the cell
         cell.contentConfiguration = content
 
         return cell
@@ -89,14 +88,6 @@ class ViewController: UITableViewController, Storyboarded {
 
         selectedFriend = friends.count - 1
         coordinator?.showViewController(FriendViewController.self, for: friend)
-    }
-
-    func updateViewController(friend: Friend) {
-        guard let selectedFriend = selectedFriend else { return }
-
-        friends[selectedFriend] = friend
-        tableView.reloadData()
-        coordinator?.saveData(friends, forKey: "Friends", errorMessage: "Unable to encode friends data.")
     }
 
 
